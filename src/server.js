4f06fa73dcf089ba424aa8c32cebbc061850ea10
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-02-21 12:39:36 +0000

    Enhancement: Restructure and update

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const User = require("./users/model");
+const userRouter = require("./users/routes");
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(userRouter);
+
+const syncTables = async () => {
+  User.sync();
+};
+
+app.listen(PORT, () => {
+  syncTables();
+  console.log(`Server is running on port ${PORT}`);
+});
